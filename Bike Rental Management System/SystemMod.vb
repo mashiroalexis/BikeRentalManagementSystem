ac7a765950dc533cb068584d404a4669c71c7f02
Ramon Alexis Celis <ramonalexiscelis@gmail.com>
2022-05-25 21:28:13 +0400

    fix merged error

--- Bike Rental Management System/SystemMod.vb
+++ Bike Rental Management System/SystemMod.vb
@@ -4,7 +4,7 @@
     '   teporary fix for connection string storage
     '   use this everytime you need to use a database connection
     '
-    Dim systemDatabaseConnectionString As String = "Data Source=ASPIRE\SQLEXPRESS;Initial Catalog=BikeRentalManagementDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+    Dim systemDatabaseConnectionString As String = "Data Source=ALEXIS\SQLEXPRESS;Initial Catalog=BikeRentalManagementSystem;Integrated Security=True"
 
 
     Public Sub roundCorners(obj As Form)
