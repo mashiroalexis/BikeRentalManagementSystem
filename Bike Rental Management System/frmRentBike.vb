aef0735352f5bfd356c98d9f4c46eb022b53647b
Ramon Alexis Celis <ramonalexiscelis@gmail.com>
2022-05-31 10:33:52 +0400

    small changes to rent

--- Bike Rental Management System/frmRentBike.vb
+++ Bike Rental Management System/frmRentBike.vb
@@ -118,48 +118,55 @@ Public Class frmRentBike
         loadRentals()
     End Sub
     Private Sub loadRentals()
-        Dim rdr As SqlDataReader = Nothing
-        Dim con As SqlConnection = New SqlConnection(getConnectionString)
-        Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM tblRent", con)
-        Dim customerFullName As String
-        con.Open()
-        rdr = cmd.ExecuteReader()
+        Try
+            Dim rdr As SqlDataReader = Nothing
+            Dim con As SqlConnection = New SqlConnection(getConnectionString)
+            Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM tblRent", con)
+            Dim customerFullName As String
+            con.Open()
+            rdr = cmd.ExecuteReader()
 
-        dgvRentalList.Rows.Clear()
-        While (rdr.Read() = True)
-            Dim rdr1 As SqlDataReader = Nothing
-            Dim con1 As SqlConnection = New SqlConnection(getConnectionString())
-            MessageBox.Show(rdr(1))
-            Dim cmd1 As SqlCommand = New SqlCommand("SELECT fullName FROM tblCustomer where Id = @id", con1)
-            cmd1.Parameters.AddWithValue("@id", rdr(1).ToString)
-            con1.Open()
-            rdr1 = cmd1.ExecuteReader()
+            dgvRentalList.Rows.Clear()
+            While (rdr.Read() = True)
+                Dim rdr1 As SqlDataReader = Nothing
+                Dim con1 As SqlConnection = New SqlConnection(getConnectionString())
+                Dim cmd1 As SqlCommand = New SqlCommand("SELECT fullName FROM tblCustomer where Id = @id", con1)
+                cmd1.Parameters.AddWithValue("@id", rdr(1).ToString)
+                con1.Open()
+                rdr1 = cmd1.ExecuteReader()
 
-            If rdr1.Read() Then
-                customerFullName = rdr1(0).ToString
-                If rdr1 IsNot Nothing Then
-                    rdr1.Close()
+                If rdr1.Read() Then
+                    customerFullName = rdr1(0).ToString
+                    If rdr1 IsNot Nothing Then
+                        rdr1.Close()
+                    End If
                 End If
-            End If
-            con1.Close()
-            dgvRentalList.Rows.Add(rdr(0), customerFullName, rdr(2), rdr(3), rdr(4), rdr(5), rdr(6))
-        End While
-        con.Close()
+                con1.Close()
+                dgvRentalList.Rows.Add(rdr(0), customerFullName, rdr(2), rdr(3), rdr(4), rdr(5), rdr(6))
+            End While
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
 
     End Sub
 
     Private Sub loadBikesList()
-        Dim rdr As SqlDataReader = Nothing
-        Dim con As SqlConnection = New SqlConnection(getConnectionString)
-        Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM tblBike", con)
-        con.Open()
-        rdr = cmd.ExecuteReader()
+        Try
+            Dim rdr As SqlDataReader = Nothing
+            Dim con As SqlConnection = New SqlConnection(getConnectionString)
+            Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM tblBike", con)
+            con.Open()
+            rdr = cmd.ExecuteReader()
 
-        clbAvailableBikes.Items.Clear()
-        While (rdr.Read() = True)
-            clbAvailableBikes.Items.Add(rdr(1))
-        End While
-        con.Close()
+            clbAvailableBikes.Items.Clear()
+            While (rdr.Read() = True)
+                clbAvailableBikes.Items.Add(rdr(1))
+            End While
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
     End Sub
 
 
