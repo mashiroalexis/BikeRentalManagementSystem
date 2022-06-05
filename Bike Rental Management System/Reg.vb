587f8c772d0d2cc267a53d0bb7942519c097e291
Ramon Alexis Celis <ramonalexiscelis@gmail.com>
2022-06-05 17:24:43 +0400

    fixed reg form

--- Bike Rental Management System/Reg.vb
+++ Bike Rental Management System/Reg.vb
@@ -5,7 +5,7 @@ Public Class Reg
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         Dim rdr As SqlDataReader = Nothing
         Dim con As SqlConnection = New SqlConnection(getConnectionString())
-        Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM tblUser username = @username", con)
+        Dim cmd As SqlCommand = New SqlCommand("SELECT username, email, contactNo from tblUser WHERE username = @username or email = @email or contactNo = @contactNo", con)
         cmd.Parameters.AddWithValue("@username", txtUsername.Text)
         cmd.Parameters.AddWithValue("@email", txtEmail.Text)
         cmd.Parameters.AddWithValue("@contactNo", txtContact.Text)
@@ -20,25 +20,12 @@ Public Class Reg
         con.Close()
     End Sub
 
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lbllogin.Click
+        Me.Close()
+        Log.Show()
+    End Sub
 
     Private Sub Reg_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         roundCorners(Me)
-    End Sub
-
-    Private Sub IconButton1_Click(sender As Object, e As EventArgs) Handles IconButton1.Click
-        Dim rdr As SqlDataReader = Nothing
-        Dim con As SqlConnection = New SqlConnection(getConnectionString())
-        Dim cmd As SqlCommand = New SqlCommand("SELECT username FROM tblUser where username = @username", con)
-        cmd.Parameters.AddWithValue("@username", txtUsername.Text)
-        con.Open()
-        rdr = cmd.ExecuteReader()
-
-        If rdr.Read() Then
-            MsgBox("success")
-        Else
-            MsgBox("failed")
-        End If
-
-
     End Sub
 End Class
